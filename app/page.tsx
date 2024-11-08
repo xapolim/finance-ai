4b3a174fde2e96946d3cf1508e84637ea009f5b2
Your Name <you@example.com>
2024-11-08 00:10:11 -0300

    add logoin

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
   return (
     <div>
       <h1 className="p-5 text-red-500">Helloweorsds</h1>;
+      <Button>helloasd</Button>
     </div>
   );
 };
